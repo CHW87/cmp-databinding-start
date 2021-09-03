1575912e2b369d45aadd01efc5bb609bd8a9f80c
CHW87 <62937643+CHW87@users.noreply.github.com>
2021-09-03 14:42:37 +0300

    practice done

--- cmp-databinding-start/src/app/app.module.ts
+++ cmp-databinding-start/src/app/app.module.ts
@@ -1,17 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 
-import { AppComponent } from './app.component';
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { ServerElementComponent } from './server-element/server-element.component';
+import {AppComponent} from './app.component';
+import {GameControlComponent} from './game-control/game-control.component';
+import {OddComponent} from './odd/odd.component';
+import {EvenComponent} from './even/even.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CockpitComponent,
-    ServerElementComponent
+    GameControlComponent,
+    OddComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule,
@@ -20,4 +22,5 @@ import { ServerElementComponent } from './server-element/server-element.componen
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
